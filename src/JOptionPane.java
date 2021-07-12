04e80ae01d658d6eba6e3bc376c7d9a63dc74caf
SreenidhiGanachari <84299719+SreenidhiGanachari@users.noreply.github.com>
2021-07-12 13:27:12 +0530

    Add files via upload

---
+++ src/JOptionPane.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sreenidhi
+ */
+class JOptionPane {
+     
+
+    static void showMessageDailog(Object object, String successfully_Updated) {
+        
+    }
+
+    static void showConfirmDailog(Object object, String incorrect_BookID) {
+         //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+
+    
+}
